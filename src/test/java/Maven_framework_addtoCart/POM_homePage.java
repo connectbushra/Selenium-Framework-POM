968ed9c3743cabfd97086ce532a9a53d27c767fc
Bushra Parween <bushraparween2017@gmail.com>
2025-05-29 16:06:31 +0530

    added extent report

--- src/test/java/Maven_framework_addtoCart/POM_homePage.java
+++ src/test/java/Maven_framework_addtoCart/POM_homePage.java
@@ -1,52 +1,78 @@
 package Maven_framework_addtoCart;
-import java.util.List;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.testng.Assert.assertTrue;
 
-import java.time.Duration;
+import java.util.List;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import dev.failsafe.internal.util.Assert;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import page_object_model.Landing_Page;
 
 public class POM_homePage {
 
-	 @Test
-	  public void addToCartTest()  throws InterruptedException {
-		// TODO Auto-generated method stub
-		String product_name="IPHONE 13 PRO";
-		WebDriverManager.chromedriver().setup(); // Auto-downloads compatible ChromeDriver
-	       
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		Landing_Page lpPage=new Landing_Page(driver);
-		lpPage.goTo();
-		lpPage.loginApplication("bush@gmail.com","Bushra@786");
-		product_catatlog product_catatlog=new product_catatlog(driver);
-		List<WebElement> products=product_catatlog.getProductList();
-	    product_catatlog.product_addtocart(product_name);
-	    product_catatlog.clickCart();
-	    CartPage cartPage=new CartPage(driver);
-//	    Boolean match=cartPage.varifyProduct(product_name);
-//	    assertTrue(match);
-	    cartPage.clickCheckout();	
-	    CheckoutPage cPage=new CheckoutPage(driver);
-	    cPage.placeOrder();
-	    String msg=cPage.successOrder();
-	    assertTrue(msg.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
-	   System.out.println("THANKYOU FOR THE ORDER.");
-	   driver.close();    
-	   driver.quit();
+    ExtentReports extent;
+    ExtentTest test;
 
-	}
+    @BeforeSuite
+    public void setupReport() {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+        reporter.config().setReportName("Automation Report");
+        reporter.config().setDocumentTitle("Test Results");
 
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Bushra");
+    }
+
+    @Test
+    public void addToCartTest() throws InterruptedException {
+        test = extent.createTest("Add To Cart Test");
+
+        String product_name = "IPHONE 13 PRO";
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        try {
+            Landing_Page lpPage = new Landing_Page(driver);
+            lpPage.goTo();
+            lpPage.loginApplication("bush@gmail.com", "Bushra@786");
+
+            product_catatlog product_catatlog = new product_catatlog(driver);
+            List<WebElement> products = product_catatlog.getProductList();
+            product_catatlog.product_addtocart(product_name);
+            product_catatlog.clickCart();
+
+            CartPage cartPage = new CartPage(driver);
+            cartPage.clickCheckout();
+
+            CheckoutPage cPage = new CheckoutPage(driver);
+            cPage.placeOrder();
+
+            String msg = cPage.successOrder();
+            assertTrue(msg.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+            test.pass("Order placed successfully");
+            System.out.println("THANKYOU FOR THE ORDER.");
+
+        } catch (Exception e) {
+            test.fail("Test failed due to: " + e.getMessage());
+            throw e;
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @AfterSuite
+    public void tearDownReport() {
+        extent.flush(); // Important: this writes the report
+    }
 }
